fb4d0a8c64dadd3bf6a011f8501ec9e384e916b2
f1she3 <f1she3@protonmail.com>
2023-12-25 23:49:01 +0100

    Refactoring & improvements

--- src/functions.py
+++ src/functions.py
@@ -1,10 +1,13 @@
 from pytickersymbols import PyTickerSymbols
 import yfinance as yf
 from datetime import date
-import pandas as pd
 
-# Returns the yahoo symbols of all companies
-# from the specified asset name (country name, or index)
+"""
+Returns the yahoo symbols of all companies
+from the specified asset name (country name, or index)
+"""
+
+
 def getCompanies(name, index=False):
     stock_data = PyTickerSymbols()
     if index:
@@ -17,39 +20,98 @@ def getCompanies(name, index=False):
         symbols = company['symbols']
         if len(company['symbols']) > 0:
             symbol = symbols[0]['yahoo']
-            companies.append((name,symbol))
+            companies.append((name, symbol))
     return companies
+
 
 def getDividends(symbols, years=1):
     dividends = []
     for symbol in symbols:
         apy = getAvgApy(symbol, years=years)
-        dividends.append(apy) 
+        dividends.append(apy)
     return dividends
+
+
+"""
+APY (yield) = dividends / start price
+"""
+
+
+def getApy(ticker, start, end):
+    hist = ticker.history(start=start, end=end)
+    apy = None
+    if not hist.empty:
+        if 'Dividends' in hist:
+            dividends = hist['Dividends'].sum()
+        else:
+            dividends = 0
+        # The price at the begining of the year (First row of the dataframe)
+        start_price = hist.iloc[0]['Open']
+        if (start_price == 0):
+            tickerName = ticker.get_info["longName"]
+            print(f"Error while calculating the APY for {tickerName}")
+        else:
+            apy = dividends / start_price
+
+    return apy
+
+
+"""
+ROI (profitability) = end price - start price + dividends / start price
+"""
+
+
+def getRoi(ticker, start, end):
+    hist = ticker.history(start=start, end=end)
+    roi = None
+    if not hist.empty:
+        if 'Dividends' in hist:
+            dividends = hist['Dividends'].sum()
+        else:
+            dividends = 0
+        # The price at the begining of the year (First row of the dataframe)
+        start_price = hist.iloc[0]['Open']
+        end_price = hist.iloc[-1]['Open']
+        if (start_price == 0):
+            tickerName = ticker.get_info["longName"]
+            print(f"Error while calculating the ROI for {tickerName}")
+        else:
+            roi = (end_price - start_price + dividends) / start_price
+
+    return roi
+
 
 """
 Returns the mean of APYs since the previous "years" years
 """
+
+
 def getAvgApy(symbol, years=5):
     ticker = yf.Ticker(symbol)
     today = date.today()
     avgApy = 0
+    total = years
     for k in range(years):
-        # The end day does not seem to be included
-        # So we do day + 1
-        start = date(today.year - years + k, today.month, today.day)
-        end = date(today.year - years + k + 1, today.month, today.day + 1)
-        hist = ticker.history(start=start, end=end)
-        if 'Dividends' in hist:
-            dividends = hist['Dividends'].sum()
+        start = today.replace(
+            year=today.year-years+k,
+            month=1,
+            # The end day does not seem to be included
+            # So we do day + 1
+            day=2
+        )
+        end = start.replace(year=start.year+1)
+        # The end date can't be in the future
+        if (end > date.today()):
+            end = date.today()
+        apy = getApy(ticker=ticker, start=start, end=end)
+        # Skip the value if the execution went wrong
+        if apy == None:
+            total -= 1
+            continue
         else:
-            dividends = None
-        avgPrice = hist['Open'].mean()
-        if dividends is not None:
-            apy = dividends/avgPrice
-            avgApy += apy * 100/years
-        else:
-            avgApy = None
+            avgApy += apy/total
             break
+    # Get a percentage
+    avgApy *= 100
 
     return avgApy
--- src/main.py
+++ src/main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
+import os
 import pandas as pd
 from functions import *
 
 # True if asset is an index, False if it's a country
 index = False
-asset = 'Spain'
+asset = 'France'
 
 companies = getCompanies(asset, index=index)
 names = []
@@ -13,7 +14,7 @@ symbols = []
 
 # Get companies names & symbols
 for company in companies:
-    name,symbol = company
+    name, symbol = company
     names.append(name)
     symbols.append(symbol)
 
@@ -28,8 +29,19 @@ df = pd.DataFrame({
     'APY (5y)': avgApys
 })
 df = df.sort_values(by=['APY (1y)'], ascending=False)
+
+base_out_dir = "output"
+final_dir = ""
+
 if index:
-    path = 'data/indices/' + asset + '.csv'
+    final_dir = "indices"
 else:
-    path = 'data/countries/' + asset + '.csv'
+    final_dir = "countries"
+
+out_dir = os.path.join(base_out_dir, final_dir)
+
+path = os.path.join(out_dir, f"{asset}.csv")
+if not os.path.exists(out_dir):
+    os.makedirs(out_dir)
+
 df.to_csv(path)
